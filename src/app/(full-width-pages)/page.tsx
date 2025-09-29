db0a17b66dfff8eb6e61caf08f79c02163e4ba5c
DanielVerissimo1 <danielSantoss1300@gmail.com>
2025-09-28 22:33:04 -0300

    feat: fazendo NavBar da landing page inicial

--- src/app/(full-width-pages)/page.tsx
+++ src/app/(full-width-pages)/page.tsx
@@ -1,12 +1,24 @@
 import Hero from "@/components/sessions-landing-page/Hero";
 import SessionsLogo from "@/components/sessions-landing-page/SessionLogos";
 import About from "@/components/sessions-landing-page/About";
+import {Header} from "@/components/NavBar";
 
 export default function Initial() {
 
   return (
 
     <div>
+        <Header
+          navItems={[
+            { href: "#sobre", label: "Sobre" },
+            { href: "#servicos", label: "Serviços" },
+            { href: "#portfolio", label: "Portfolio" },
+            { href: "#contato", label: "Contato" }
+          ]}
+          ctaText="Faça Login"
+          ctaHref="#contato"
+          mobileCtaText="Começar"
+        />
 
         <Hero/>
 

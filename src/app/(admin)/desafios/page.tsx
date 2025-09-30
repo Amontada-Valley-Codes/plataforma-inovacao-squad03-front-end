733252e381b6fbc84ddae1a849949fff6ab9e3a4
DanielVerissimo1 <danielSantoss1300@gmail.com>
2025-09-30 19:40:36 -0300

    refactor: sideBar layout

---
+++ src/app/(admin)/desafios/page.tsx
@@ -0,0 +1,8 @@
+export default function page(){
+    return(
+        <div className="flex flex-col justify-center items-center h-screen">
+            
+            <p className="text-xl">Page </p>
+        </div>
+    )
+}

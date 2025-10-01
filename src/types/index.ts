62b94ddb6740f332fdac1aaffd47fc565ea522ad
Samuel Melo <samuelmelo13789@gmail.com>
2025-10-01 20:43:15 -0300

    feat: Criação do card public exibidos na página de workflow

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type PropsCard = {
+
+    image: string;
+    corporationName: string;
+    startDate: string;
+    finishDate: string;
+    title: string;
+    description: string;
+    sector: string;
+    public: boolean
+
+}

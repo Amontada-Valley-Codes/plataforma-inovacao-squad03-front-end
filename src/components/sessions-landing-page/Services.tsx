508a19ab4538ec7842d0071fe57e2ca04a33d6ea
Giovanna <gg.aventureira@gmail.com>
2025-10-01 19:50:08 -0300

    Estilização

--- src/components/sessions-landing-page/Services.tsx
+++ src/components/sessions-landing-page/Services.tsx
@@ -3,7 +3,7 @@ export default function Services() {
   return (
 
     <div
-      className="w-full min-h-screen bg-[#F5F5F5] 
+      className="w-full min-h-screen bg-white
                  bg-[url('/imagemfundo1.png')] bg-cover bg-center 
                  flex items-center py-12 sm:py-16 md:py-20"
     >
@@ -14,57 +14,57 @@ export default function Services() {
         >
       
             <div>
-            <h2 className="text-[#0E0062] text-4x1 sm:text-4xl font-bold flex items-center">
-                <span className="h-6 w-1.5 bg-[#83C231] mr-3" />
+            <h2 className="text-blue text-4x1 sm:text-4xl font-semibold flex items-center">
+                <span className="h-15 w-2.5 bg-green mr-3" />
                 Gestão de desafios
             </h2>
-            <p className="text-[#6F6F6F] mt-2 text-sm sm:text-[20px]">
+            <p className="text-gray mt-2 text-sm sm:text-[20px] ml-6">
                 Publique problemas estratégicos e receba propostas.
             </p>
             </div>
 
        
             <div>
-            <h2 className="text-[#0E0062] text-4x1 sm:text-4xl font-bold flex items-center">
-                <span className="h-6 w-1.5 bg-[#83C231] mr-3" />
+            <h2 className="text-blue text-4x1 sm:text-4xl font-semibold flex items-center">
+                <span className="h-15 w-2.5 bg-green mr-3" />
                 Avaliação e seleção colaborativa
             </h2>
-            <p className="text-[#6F6F6F] mt-2 text-sm sm:text-[20px]">
+            <p className="text-gray mt-2 text-sm sm:text-[20px] ml-6">
                 Ambiente de avaliação e feedback estruturado.
             </p>
             </div>
 
      
             <div>
-            <h2 className="text-[#0E0062] text-4x1 sm:text-4xl font-bold flex items-center mt-6">
-                <span className="h-6 w-1.5 bg-[#83C231] mr-3" />
+            <h2 className="text-blue text-4x1 sm:text-4xl font-semibold flex items-center mt-6">
+                <span className="h-15 w-2.5 bg-green mr-3" />
                 Banco de ideias e soluções
             </h2>
-            <p className="text-[#6F6F6F] mt-2 text-sm sm:text-[20px]">
+            <p className="text-gray mt-2 text-sm sm:text-[20px] ml-6">
                 Centralize ideias, utilizando de avaliação e priorização.
             </p>
             </div>
 
             <div>
-            <h2 className="text-[#0E0062] text-4x1 sm:text-4xl font-bold flex items-center mt-6">
-                <span className="h-6 w-1.5 bg-[#83C231] mr-3" />
+            <h2 className="text-blue text-4x1 sm:text-4xl font-semibold flex items-center mt-6">
+                <span className="h-15 w-2.5 bg-green mr-3" />
                 Comunicação e engajamento
             </h2>
-            <p className="text-[#6F6F6F] mt-2 text-sm sm:text-[20px]">
+            <p className="text-gray mt-2 text-sm sm:text-[20px] ml-6">
                 Dashboard, organização kanban e ambiente de comentários.
             </p>
             </div>
 
             <div>
-            <h2 className="text-[#0E0062] text-4x1 sm:text-4xl font-bold flex items-center mt-5">
-                <span className="h-6 w-1.5 bg-[#83C231] mr-3" />
+            <h2 className="text-blue text-4x1 sm:text-[32px] font-semibold flex items-center mt-5">
+                <span className="h-15 w-2.5 bg-green mr-3" />
                 Matchmaking
             </h2>
-            <p className="text-[#6F6F6F] mt-2 text-sm sm:text-[20px]">
+            <p className="text-gray mt-2 text-sm sm:text-[20px] ml-6">
                 Filtros que aproximam desafios de soluções com startups e parceiros.
             </p>
             </div>
+        </div>
     </div>
-</div>
   );
 }

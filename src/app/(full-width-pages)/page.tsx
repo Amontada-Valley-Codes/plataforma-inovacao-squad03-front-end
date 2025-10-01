44be231dc61fc698538392e2832754dbb2fb69ad
Giovanna <gg.aventureira@gmail.com>
2025-09-30 21:37:49 -0300

    Image

--- src/app/(full-width-pages)/page.tsx
+++ src/app/(full-width-pages)/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/sessions-landing-page/Hero";
 import SessionsLogo from "@/components/sessions-landing-page/SessionLogos";
 import About from "@/components/sessions-landing-page/About";
 import {Header} from "@/components/NavBar";
+import Services from "@/components/sessions-landing-page/Services";
 
 export default function Initial() {
 
@@ -25,6 +26,8 @@ export default function Initial() {
         <SessionsLogo/>
 
         <About/>
+
+        <Services/>
 
     </div>
     
--- src/components/sessions-landing-page/Services.tsx
+++ src/components/sessions-landing-page/Services.tsx
@@ -1,1 +1,24 @@
-'use client'
+import Image from "next/image";
+
+export default function Services() {
+
+    return (
+
+        <div className="flex flex-col lg:flex-row w-full min-h-screen bg-[#F5F5F5]">
+
+                {/* Image */}
+                <div className="w-full flex justify-center items-center"> 
+
+                    <Image
+                        src={"/imagemfundo1.png"}
+                        alt="Imagem de fundo - Services"
+                        width={800}
+                        height={800}
+                        className="w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-screen object-cover"
+                    />
+
+                </div>                 
+
+        </div>
+    )
+}

4eb89acca4afcbb12505cc5ed76e016908bdacf8
Giovanna <gg.aventureira@gmail.com>
2025-10-01 21:21:06 -0300

    feat: imagem de fundo

---
+++ src/components/sessions-landing-page/Startups.tsx
@@ -0,0 +1,16 @@
+
+export default function Startups() {
+
+    return(
+       
+        <div className="w-full min-h-screen opacity-65
+                        bg-[url('/imagemfundo2.png')] bg-cover bg-center 
+                        flex items-center py-12 sm:py-16 md:py-20">
+
+            <div className="w-full h-full flex bg-blue/80" ></div>
+            
+        </div>
+
+    )
+
+}

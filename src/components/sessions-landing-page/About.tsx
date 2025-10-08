52e1b8f62781014efe430338274ef824743b59a6
Andressa <contatoandressaskm@gmail.com>
2025-10-07 21:36:50 -0300

    texto section about

--- src/components/sessions-landing-page/About.tsx
+++ src/components/sessions-landing-page/About.tsx
@@ -2,13 +2,13 @@ import Image from "next/image"
 
 export default function About() {
   return (
-    <section className="flex flex-col lg:flex-row w-full min-h-screen bg-[#FFFFFF]">
+    <section className="relative flex flex-col lg:flex-row w-full min-h-screen bg-[#FFFFFF]">
       
       {/* Lado da Imagem */}
-      <div className="relative w-full h-[300px] lg:w-1/2 lg:h-screen">
+      <div className="relative w-full h-[300px] lg:w-1/2 lg:h-screen z-0">
         <Image
           src="/Imagem_2.png" 
-          alt="Sobre a plataforma de inovação"
+          alt="Sobre a plataforma de inovação."
           fill
           quality={100}
           className="object-cover"
@@ -17,14 +17,14 @@ export default function About() {
       </div>
 
       {/* Lado do Texto */}
-      <div className="flex flex-col justify-center w-full lg:w-1/2 px-6 lg:px-12 py-10">
-            <div className="max-w-[500px] mx-auto">
+      <div className="flex flex-col justify-normal w-full lg:w-1/2 px-6 lg:px-12 py-10 mt-20 font-light z-0">
+            <div className="max-w-[480px] mx-auto">
 
-                <p className="text-[16px] text-justify tracking-wide md:text-[20px] lg:text-[20px] text-gray-600 leading-relaxed">
+                <p className="text-[16px] text-justify tracking-wide md:text-[20px] lg:text-[24px] text-gray-600 leading-relaxed">
                 Ferramenta que nasceu com o objetivo de conectar pessoas e projetos, 
                 para criar <span className="font-bold text-green">soluções</span> de forma inteligente e integrada.
                 </p>
-                <p className="text-[16px] text-justify md:text-[20px] lg:text-[20px] text-gray-600 leading-relaxed mt-10">
+                <p className="text-[16px] text-justify md:text-[20px] lg:text-[24px] text-gray-600 leading-relaxed mt-10">
                 Nossa plataforma reúne as ferramentas necessárias para transformar a 
                 organização interna dos seus projetos e gerar um ambiente de interação 
                 que acelera a resolução de problemas.
@@ -32,6 +32,24 @@ export default function About() {
             </div>
         </div>
 
+        <div className="absolute top-1/2 left-[40%] lg:left-[8%] -translate-y-1/2 z-10 pointer-events-none"
+             aria-hidden="true">
+          <Image
+          src="/Imagem1.png" 
+          alt="Mockup Dashboard."
+          width={800}
+          height={800}
+          quality={100}
+          className="object-contain"
+          priority
+        />          
+        </div>
+
+        <div  className="absolute right-1/7 rounded-t-4xl bottom-0 w-75 h-52 bg-green">
+            <p className="text-white justify-center font-medium text-[30px] mt-5 ml-10 leading-10">
+              Conheça o que nosso sistema oferece
+            </p>
+          </div>
     </section>
   )
 }

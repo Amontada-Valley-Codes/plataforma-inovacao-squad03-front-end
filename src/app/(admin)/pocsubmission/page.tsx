a2235c0189ef14f3b59fa336f91bf14b006d3fca
Andressa <contatoandressaskm@gmail.com>
2025-10-14 19:29:59 -0300

    poc submission page

---
+++ src/app/(admin)/pocsubmission/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import SubmissionForm from "@/components/elements/SubmissionPOC/SubmissionForm"
+
+export default function Submission() {
+    return (
+        <div className="flex items-center justify-center py-4 w-full">
+            <SubmissionForm/>
+        </div>
+    )
+}
+

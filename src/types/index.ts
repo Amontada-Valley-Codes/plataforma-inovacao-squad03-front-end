9fe87d613112725376869f36ad81879d24fbe18f
Samuel Melo <samuelmelo13789@gmail.com>
2025-10-16 19:51:07 -0300

    feat: implementação de um ajuste no reload de desafios com base em ações

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,5 +16,31 @@ export type PropsCard = {
             public_id: string
         }
     }
+}
 
+export type PropsCardKanban = {
+
+    id: string;
+    name: string;
+    startDate: string;
+    endDate: string;
+    sector: string;
+    description: string;
+    publishOption: "PUBLIC" | "RESTRICTED";
+    corporationId: string;
+    status: string;
+    corporation: {
+        tradingName: string
+        logo: {
+            url: string
+            public_id: string
+        }
+    }
+    setReload: (prev: boolean) => void;
+    realod: boolean
+}
+
+export type PropsFormChallenger = {
+    setReload: (prev: boolean) => void;
+    realod: boolean
 }

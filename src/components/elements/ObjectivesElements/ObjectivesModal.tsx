cb0039bdd0a70ae0d603b83dc933d5bfa1f7998d
Samuel Melo <samuelmelo13789@gmail.com>
2026-01-12 15:53:06 -0300

    feat: Finalizado o frontend da sessão de objetivos estratégicos

--- src/components/elements/ObjectivesElements/ObjectivesModal.tsx
+++ src/components/elements/ObjectivesElements/ObjectivesModal.tsx
@@ -9,6 +9,17 @@ import CardObjective from "./CardObjective";
 export default function ObjectivesModal() {
   const { isOpen, openModal, closeModal } = useModal();
 
+  const objectives = [
+    {
+      content: "Algum objetivo que eles cria aí.",
+      createDate: "01/10/26"
+    },
+    {
+      content: "Outro objetivo que eles cria aí.",
+      createDate: "01/10/26"
+    },
+    
+  ]
 
   return (
     <div>
@@ -23,18 +34,20 @@ export default function ObjectivesModal() {
         <Modal
           isOpen={isOpen}
           onClose={closeModal}
-          className="max-w-[600px] p-5 lg:p-10 mx-2"
+          className="max-w-[600px] max-h-[600px] p-5 lg:p-10 mx-2"
         >
             <div className="mb-4 border-b-2 py-2">
+
                 <h4 className="font-semibold text-blue text-[25px]">
                     Objetivos Estratégicos
                 </h4>
                 <span className="text-md leading-6 text-gray-500">
                     Crie e edite os objetivos estratégicos da sua empresa
                 </span>
+
             </div>
 
-            <div>
+            <div className="space-y-4">
 
                 <div className="py-2">
                   
@@ -42,9 +55,14 @@ export default function ObjectivesModal() {
 
                 </div>
 
-                <div>
-                    
-                    <CardObjective/>
+                <div className="max-h-[300px] scrollbar-hidden overflow-auto space-y-2">
+                    {objectives.map((objective, index) => (
+                      <CardObjective
+                        key={index}
+                        content={objective.content}
+                        createDate={objective.createDate}
+                      />
+                    ))}
 
                 </div>
 

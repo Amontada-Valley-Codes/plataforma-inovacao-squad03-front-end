cfdf212987be89746f15ab0ac2caf2d115360126
Samuel Melo <samuelmelo13789@gmail.com>
2026-01-14 15:15:08 -0300

    feat: Finaizado a conexão dos objetivos estratégicos com as chamadas da api

--- src/components/elements/ObjectivesElements/ObjectivesModal.tsx
+++ src/components/elements/ObjectivesElements/ObjectivesModal.tsx
@@ -7,6 +7,7 @@ import { useModal } from "@/hooks/useModal";
 import InputObjective from "./InputObjective";
 import CardObjective from "./CardObjective";
 import { CardObjectiveProps } from "@/types";
+import { api } from "@/api/axiosConfig";
 
 export default function ObjectivesModal() {
   const { isOpen, openModal, closeModal } = useModal();
@@ -17,7 +18,17 @@ export default function ObjectivesModal() {
 
     const getObjectives = async () => {
 
+      const token = localStorage.getItem("authtoken")
 
+      const response = await api.get("/strategic-objectives", 
+        {
+          headers: {
+              Authorization: `Bearer ${token}`
+          }
+        }
+      )
+
+      setObjectives(response.data)
 
     }
     getObjectives()
@@ -70,8 +81,8 @@ export default function ObjectivesModal() {
                       <CardObjective
                         key={index}
                         id={objective.id}
-                        content={objective.content}
-                        createDate={objective.createDate}
+                        title={objective.title}
+                        createdAt={objective.createdAt}
                         ObjectiveUpload={objectiveUpload}
                         setObjectUpload={setObjectUpload}
                       />
